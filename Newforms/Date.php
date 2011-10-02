22a6779ab39a9bd3a5ecca4cf7c85aa747677489
Vlad Andersen <vlad.andersen@gmail.com>
2011-10-02 21:15:26 +0400

    Setting default delta in Newforms_Date and a preference for showing minutes.

--- Newforms/Date.php
+++ Newforms/Date.php
@@ -4,8 +4,13 @@ require_once '__Field.php';
 
 class lib_Newforms_Date extends lib_Newforms___Field {
 
+    public $show_minutes = true;
+    public $delta = 0;
+
     public function html() {
         $date = strtotime ($this->__value);
+        if (!$this->__value) $date = time() + $this->delta;
+        
         $day = date ('d', $date);
         $month = date ('m', $date);
         $year = date ('Y', $date);
@@ -33,8 +38,10 @@ class lib_Newforms_Date extends lib_Newforms___Field {
                 <option <?= $i == $year ? 'selected' : '' ?> value="<?= $i ?>"><?= $i ?></option>
                 <?php } ?>
             </select>
-            <span style="padding-right: 10px">,</span>
             
+            
+            <?php if ($this->show_minutes) { ?>
+            <span style="padding-right: 10px">,</span>
             <select name="<?= $this->__name ?>_hour" id="newforms_<?= $this->__name ?>_hour">
                 <?php for ($i = 0; $i <= 23; $i ++) { ?>
                 <option <?= $i == $hour ? 'selected' : '' ?> value="<?= $i ?>"><?= str_pad($i, 2, '0', STR_PAD_LEFT) ?></option>
@@ -46,6 +53,8 @@ class lib_Newforms_Date extends lib_Newforms___Field {
                 <option <?= $i == $minute ? 'selected' : '' ?> value="<?= $i ?>"><?= str_pad($i, 2, '0', STR_PAD_LEFT) ?></option>
                 <?php } ?>
             </select>
+        <?php } ?>
+        <div style="height: 3px"><!-- --></div>
                   
         
         <?php return ob_get_clean();
@@ -57,13 +66,19 @@ class lib_Newforms_Date extends lib_Newforms___Field {
           $day    = $_POST[$this->__name . '_day'];
           $month  = $_POST[$this->__name . '_month'];
           $year   = $_POST[$this->__name . '_year'];
-          $hour   = $_POST[$this->__name . '_hour'];
-          $minute = $_POST[$this->__name . '_minutes'];
           $day = str_pad($day, 2, '0', STR_PAD_LEFT);
           $month = str_pad($month, 2, '0', STR_PAD_LEFT);
-          $hour = str_pad($hour, 2, '0', STR_PAD_LEFT);
-          $minute = str_pad($minute, 2, '0', STR_PAD_LEFT);
-          $this->__value = sprintf ('%s-%s-%s %s:%s:00', $year, $month, $day, $hour, $minute);
+          if ($this->show_minutes) { 
+              $hour   = $_POST[$this->__name . '_hour'];
+              $minute = $_POST[$this->__name . '_minutes'];
+              $hour = str_pad($hour, 2, '0', STR_PAD_LEFT);
+              $minute = str_pad($minute, 2, '0', STR_PAD_LEFT);
+              $this->__value = sprintf ('%s-%s-%s %s:%s:00', $year, $month, $day, $hour, $minute);
+          } else {
+              $this->__value = sprintf ('%s-%s-%s', $year, $month, $day);
+          }
+          
+          
       }
 
     }    
